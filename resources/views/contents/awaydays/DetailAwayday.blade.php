8eb5959281132ccbec2ebaf32697c02fa1620049
errww <erwinarvi88@gmail.com>
2018-12-27 01:14:37 +0700

    detail awayday

--- resources/views/contents/awaydays/DetailAwayday.blade.php
+++ resources/views/contents/awaydays/DetailAwayday.blade.php
@@ -13,6 +13,56 @@
                             {{ session('status') }}
                         </div>
                     @endif
+
+                    <p class="lead text-center">Lorem Ipsum Dolor <span class="fa fa-check-circle"></span>
+                    </p>
+                    <p class="lead text-center">[ Match P vs K ]</p>
+                    <p class="text-center">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                    <ul class="list-inline text-center">
+                      <li class="list-inline-item">
+                        <button type="button" class="btn btn-outline-success"> Match : 11/01/2019</button>
+                      </li>
+                      <li class="list-inline-item">
+                        <button type="button" class="btn btn-outline-success">Open Slot: 400</button>
+                      </li>
+                      <li class="list-inline-item">
+                        <button type="button" class="btn btn-outline-success">Biaya : 200.000</button>
+                      </li>
+                      <li class="list-inline-item">
+                        <button type="button" class="btn btn-outline-success">Tutup : 09/01/2019</button>
+                      </li>
+                    </ul>
+                    
+                    <hr>
+
+                    <table class="table table-bordered">
+                      <tbody>
+                        <tr>
+                          <td>Slot Tersisa :</td>
+                          <td>0</td>
+                        </tr>
+                        <tr>
+                          <td>Total Bis :</td>
+                          <td>20</td>
+                        </tr>
+                        <tr>
+                          <td>Total Mobil :</td>
+                          <td>20</td>
+                        </tr>
+                        <tr>
+                          <td>Total Sepeda Motor :</td>
+                          <td>1000</td>
+                        </tr>
+                      </tbody>
+                    </table>
+
                    
                 </div>
             </div>
